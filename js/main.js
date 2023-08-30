1391c22efd3b41db97d36fd6086f4a68f9711964
Trent Menard <trentmenard6@yahoo.com>
2023-08-30 00:32:52 -0500

    New css/html design, JS book entry auto-creation from arr

--- js/main.js
+++ js/main.js
@@ -23,10 +23,9 @@ const ATaleOfTwoCities = new Book("A Tale Of Two Cities", "Charles Dickens");
 addBookToLibrary(ATaleOfTwoCities);
 
 const TheLittlePrince = new Book("The Little Prince", "Antoine de Saint-Exupéry");
-addBookToLibrary(LordOfTheRings);
+addBookToLibrary(TheLittlePrince);
 
-const newBookDialog = document.querySelector("#new-book-dialog");
-
+/*const newBookDialog = document.querySelector("#new-book-dialog");
 
 const newBookButton = document.querySelector("#new-book-button");
 newBookButton.addEventListener("click", () => {
@@ -34,4 +33,30 @@ newBookButton.addEventListener("click", () => {
 });
 
 const cancelButton = document.querySelector("#cancel-btn");
-cancelButton.addEventListener("close", (e) => console.log(e));
+cancelButton.addEventListener("close", (e) => console.log(e));*/
+
+myLibrary.forEach(b => {
+    const bookInfoDiv = document.createElement("div");
+    bookInfoDiv.classList.add("book-info-div");
+    
+    const bookImg = document.createElement("img");
+    bookImg.src = "./img/book.svg";
+    bookImg.alt = "book";
+    bookImg.style.width = "125px";
+    bookImg.style.height = "125px";
+    
+    bookInfoDiv.append(bookImg);
+
+    const bookInfoSubDiv = document.createElement("div");
+    
+    const bookInfoH2 = document.createElement("h2");
+    bookInfoH2.textContent = b.title;
+    bookInfoH2.style.fontStyle = "italic";
+
+    const bookInfoH3 = document.createElement("h3");
+    bookInfoH3.textContent = b.author;
+
+    bookInfoSubDiv.append(bookInfoH2, bookInfoH3);
+    bookInfoDiv.append(bookInfoSubDiv);
+    document.querySelector("#library-div").append(bookInfoDiv);
+})
